dd06a2ee6094278a24f33c8803d85eb2b664bb45
cf27675@gmail.com <cf27675@gmail.com@a0b708d1-0eea-223f-8d9c-093d4dda640a>
2015-02-24 21:42:26 +0000

    added ads support for android

--- CN1Flurry/native/win/com/codename1/flurry/FlurryNativeImpl.cs
+++ CN1Flurry/native/win/com/codename1/flurry/FlurryNativeImpl.cs
@@ -7,28 +7,50 @@ public class FlurryNativeImpl {
     public void logEvent(String param, bool param1) {
     }
 
-    public void initFlurry(String param) {
-    }
-
     public void setAge(int param) {
-    }
-
-    public void onPageView() {
-    }
-
-    public void setLogEvents(bool param) {
-    }
-
-    public void setGender(String param) {
     }
 
     public void setUserID(String param) {
     }
 
-    public void setCrashReportingEnabled(bool param) {
+    public void endSession() {
+    }
+
+    public void initFlurry(String param) {
+    }
+
+    public bool isAdReady() {
+        return false;
+    }
+
+    public void setLogEvents(bool param) {
+    }
+
+    public void displayAd() {
+    }
+
+    public void onPageView() {
+    }
+
+    public void setGender(String param) {
+    }
+
+    public void destroyAd() {
+    }
+
+    public void fetchAd() {
+    }
+
+    public void startSession() {
+    }
+
+    public void setAdSpaceName(String param) {
     }
 
     public void endTimedEvent(String param) {
+    }
+
+    public void setCrashReportingEnabled(bool param) {
     }
 
     public bool isSupported() {
