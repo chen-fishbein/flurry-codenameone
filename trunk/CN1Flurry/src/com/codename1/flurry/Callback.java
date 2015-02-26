37848d763398504b8a5b1fae8ebd5604ae2bfc5d
steve.hannah@codenameone.com <steve.hannah@codenameone.com@a0b708d1-0eea-223f-8d9c-093d4dda640a>
2015-02-26 04:12:28 +0000

    Wrapped listener callbacks in callSerially() so that they are run on the EDT as is the convention for most listener events in CN1.

--- trunk/CN1Flurry/src/com/codename1/flurry/Callback.java
+++ trunk/CN1Flurry/src/com/codename1/flurry/Callback.java
@@ -5,6 +5,8 @@
  */
 package com.codename1.flurry;
 
+import com.codename1.ui.Display;
+
 /**
  *
  * @author Chen
@@ -15,43 +17,78 @@ public class Callback {
     
     public static void onClicked(){
         if(l != null){
-            l.onClicked();
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onClicked();
+                }
+            });
         }
     }
 
     public static void onClose(){
         if(l != null){
-            l.onClose();
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onClose();
+                }
+            });
         }    
     }
 
     public static void onDisplay(){
         if(l != null){
-            l.onDisplay();
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onDisplay();
+                }
+            });
         }        
     }
 
     public static void onAppExit(){
         if(l != null){
-            l.onAppExit();
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onAppExit();
+                }
+            });
         }            
     }
 
     public static void onRendered(){
         if(l != null){
-            l.onRendered();
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onRendered();
+                }
+            });
         }                
     }
 
-    public static void onError(String err){
+    public static void onError(final String err){
         if(l != null){
-            l.onError(err);
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onError(err);
+                }
+            });
         }                    
     }
 
     public static void onFetched(){
         if(l != null){
-            l.onFetched();
+            Display.getInstance().callSerially(new Runnable(){
+
+                public void run() {
+                    l.onFetched();
+                }
+            });
         }                        
     }
     
