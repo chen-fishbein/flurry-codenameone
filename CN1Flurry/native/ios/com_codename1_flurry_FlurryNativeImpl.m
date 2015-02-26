2726aa5ff1ceaa83b2c13470cb1c76876c0b5bb7
steve.hannah@codenameone.com <steve.hannah@codenameone.com@a0b708d1-0eea-223f-8d9c-093d4dda640a>
2015-02-26 01:16:15 +0000

    An initial non-functional implementation of iOS port for the Flurry lib.

--- CN1Flurry/native/ios/com_codename1_flurry_FlurryNativeImpl.m
+++ CN1Flurry/native/ios/com_codename1_flurry_FlurryNativeImpl.m
@@ -22,6 +22,7 @@ FlurryAdInterstitial *adInterstitial = nil;
 
 -(void)initFlurry:(NSString*)param{
     adID = param;
+    
 }
 
 -(void)startSession{
@@ -53,11 +54,18 @@ FlurryAdInterstitial *adInterstitial = nil;
 }
 
 -(BOOL)isAdReady{
+    if (adInterstitial == nil){
+        return NO;
+    }
     return [adInterstitial ready];
 }
 
 -(void)setAdSpaceName:(NSString*)param{
-    adInterstitial = [[FlurryAdInterstitial alloc]  initWithSpace:param];
+    id del = self;
+    dispatch_sync(dispatch_get_main_queue(), ^{
+        adInterstitial = [[FlurryAdInterstitial alloc]  initWithSpace:param];
+        adInterstitial.adDelegate = del;
+    });
 }
 
 -(void)fetchAd{
@@ -73,8 +81,127 @@ FlurryAdInterstitial *adInterstitial = nil;
 }
 
 
+
 -(BOOL)isSupported{
     return YES;
 }
+
+
+/*!
+ *  @brief Invoked when an ad is received for the specified @c interstitialAd object.
+ *  @since 6.0.0
+ *
+ *  This method informs the app that an ad has been received and is available for display.
+ *
+ *  @see FlurryAdInterstitial#fetchAd for details on the method that will invoke this delegate.
+ *
+ *  @param interstitialAd The ad object that has successfully fetched an ad.
+ */
+- (void) adInterstitialDidFetchAd:(FlurryAdInterstitial*)interstitialAd{
+    com_codename1_flurry_Callback_onFetched__(CN1_THREAD_GET_STATE_PASS_SINGLE_ARG);
+}
+
+/*!
+ *  @brief Invoked when the interstitial ad is rendered.
+ *  @since 6.0.0
+ *
+ *  This method informs the user an ad was retrieved, and successful in displaying to the user.
+ *
+ *  @see \n
+ *  FlurryAdInterstitial#presentWithViewController: for details on the method that will invoke this delegate. \n
+ *
+ *  @param interstitialAd The ad object that rendered successfully.
+ *
+ */
+- (void) adInterstitialDidRender:(FlurryAdInterstitial*)interstitialAd{
+    com_codename1_flurry_Callback_onRendered__(CN1_THREAD_GET_STATE_PASS_SINGLE_ARG);
+}
+
+/*!
+ *  @brief Invoked when a fullscreen associated with the specified ad will present on the screen.
+ *  @since 6.0.0
+ *
+ *  @param interstitialAd The interstitial ad object that is associated with the full screen that will present.
+ *
+ */
+- (void) adInterstitialWillPresent:(FlurryAdInterstitial*)interstitialAd{
+    com_codename1_flurry_Callback_onDisplay__(CN1_THREAD_GET_STATE_PASS_SINGLE_ARG);
+}
+
+
+/*!
+ *  @brief Invoked when the ad has been selected that will take the user out of the app.
+ *  @since 6.0.0
+ *
+ *  This method informs the app that an ad has been clicked and the user is about to be taken outside the app.
+ *
+ *  @param interstitialAd The ad object that received the click.
+ *
+ */
+- (void) adInterstitialWillLeaveApplication:(FlurryAdInterstitial*)interstitialAd{
+    com_codename1_flurry_Callback_onAppExit__(CN1_THREAD_GET_STATE_PASS_SINGLE_ARG);
+}
+
+/*!
+ *  @brief Invoked when a fullscreen associated with the specified ad will be removed.
+ *  @since 6.0.0
+ *
+ *  @param interstitialAd The interstitial ad object that is associated with the full screen that will be dismissed.
+ *
+ */
+- (void) adInterstitialWillDismiss:(FlurryAdInterstitial*)interstitialAd{}
+
+/*!
+ *  @brief Invoked when a fullscreen associated with the specified ad has been removed.
+ *  @since 6.0.0
+ *
+ *  @param interstitialAd The interstitial ad object that is associated with the full screen that has been dismissed.
+ *
+ */
+- (void) adInterstitialDidDismiss:(FlurryAdInterstitial*)interstitialAd{
+    com_codename1_flurry_Callback_onClose__(CN1_THREAD_GET_STATE_PASS_SINGLE_ARG);
+}
+
+/*!
+ *  @brief Informational callback invoked when an ad is clicked for the specified @c interstitialAd object.
+ *  @since 6.0.0
+ *
+ *  This method informs the app that an ad has been clicked. This should not be used to adjust state of an app. It is only intended for informational purposes.
+ *
+ *  @param interstitialAd The ad object that received the click.
+ *
+ */
+- (void) adInterstitialDidReceiveClick:(FlurryAdInterstitial*)interstitialAd{
+    com_codename1_flurry_Callback_onClicked__(CN1_THREAD_GET_STATE_PASS_SINGLE_ARG);
+}
+
+/*!
+ *  @brief Invoked when a video finishes playing
+ *  @since 6.0.0
+ *
+ *  This method informs the app that a video associated with this ad has finished playing.
+ *
+ *  @param interstitialAd The interstitial ad object that played the video and finished playing the video.
+ *
+ */
+- (void) adInterstitialVideoDidFinish:(FlurryAdInterstitial*)interstitialAd{}
+
+/*!
+ *  @brief Informational callback invoked when there is an ad error
+ *  @since 6.0
+ *
+ *  @see FlurryAdError for the possible error reasons.
+ *
+ *  @param interstitialAd The interstitial ad object associated with the error
+ *  @param adError an enum that gives the reason for the error.
+ *  @param errorDescription An error object that gives additional information on the cause of the ad error.
+ *
+ */
+- (void) adInterstitial:(FlurryAdInterstitial*) interstitialAd adError:(FlurryAdError) adError errorDescription:(NSError*) errorDescription{
+    NSLog(@"Error with interstitial ad: %@", errorDescription.localizedDescription);
+
+    com_codename1_flurry_Callback_onError___java_lang_String(CN1_THREAD_GET_STATE_PASS_ARG fromNSString(CN1_THREAD_GET_STATE_PASS_ARG [errorDescription localizedDescription]));
+}
+
 
 @end
